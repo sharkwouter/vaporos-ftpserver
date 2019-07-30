aa3f5bc22aefdebed2e533d01702319e084d4718
Wouter Wijsman <s1106648@student.windesheim.nl>
2019-07-30 15:58:55 +0200

    Added main check

--- vaportransport.py
+++ vaportransport.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
-from vaportransport import GUI, Server
 import thread, os
 
-# Set the working directory
-file_name = os.path.realpath(__file__)
-directory_name = os.path.dirname(os.path.abspath(file_name))
-os.chdir(directory_name)
+if __name__ == "__main__":
+  from vaportransport import GUI, Server
+  # Setup the FTP server and gui
+  port = 2020
+  server = Server(port)
+  gui = GUI(server)
 
-# Setup the FTP server and gui
-port = 2020
-server = Server(port)
-gui = GUI(server)
-
-# Run the server
-try:
-    thread.start_new_thread(server.run, ())
-except:
+  # Run the server
+  try:
+   thread.start_new_thread(server.run, ())
+  except:
     print("Error: unable to start server thread")
 
-# Run the GUI
-gui.run()
+  # Run the GUI
+  gui.run()
